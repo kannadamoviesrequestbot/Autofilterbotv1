cc016b7ec7490ecab9cacae01cef9202baa9e917
kannadamoviesrequestbot <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2021-08-09 10:47:05 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @KANNADAMOVIES_17", 
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    ' 🙈our channel🙈 ', url="https://t.me/searchkannadamovies"
                                 )
                         ]
                     ]
