fe51586bd2c7b7e1ed119b57a8d875dcc4a22837
kannadamoviesrequestbot <88633158+kannadamoviesrequestbot@users.noreply.github.com>
2021-08-09 10:38:59 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b><u>Hey {} How are you!!</b></u>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+You can't add me to ☺ your Group👅.. 
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@searchkannadamovies
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,15 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Beereshbanakar">kannadamoviesrequestbot</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/kannadamoviesrequestbot/Autofilterbotv1">Click Me</a></i>
 """
